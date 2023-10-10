bf6245adfecbcde8463a7b6043baebadf19fbb6b
DanielaE1512 <d.echavarria@uniandes.edu.co>
2023-10-09 20:36:48 -0500

    Adelanto sede

---
+++ Entrega-3/ProyectoEclipse/src/model/Sede.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.time.LocalTime;
+
+public class Sede {
+	
+	//Atributos
+	
+	private String nombre;
+	
+	private String ubicacion;
+	
+	private LocalTime inicio;
+	
+	private LocalTime fin;
+	
+	//Constructor
+	
+	public Sede (String nombre, String ubicacion, LocalTime inicio, LocalTime fin)
+	{
+		this.nombre = nombre;
+		this.ubicacion= ubicacion;
+		this.inicio = inicio;
+		this.fin = fin;
+		
+	}
+	
+	//Métodos consultar información
+	
+	String getNombre()
+	{
+		return this.nombre;
+	}
+	
+	String getUbicacion()
+	{
+		return this.ubicacion;
+	}
+	
+	LocalTime getInicio()
+	{
+		return this.inicio;
+	}
+	
+	LocalTime getFin()
+	{
+		return this.fin;
+	}
+	
+	
+	
+
+}
