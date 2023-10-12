6c9cbd681ca8db26ec15deb2a4f023b301e3f946
dacalderonp <dacalderonp@protonmail.com>
2023-10-12 10:10:43 -0500

    Cambios en Usuarios y persistencia en registro clientes

--- Entrega-3/ProyectoEclipse/src/model/Usuario.java
+++ Entrega-3/ProyectoEclipse/src/model/Usuario.java
@@ -1,9 +1,16 @@
 package model;
 
 public interface Usuario {
+	    
 	public String getUsuario();
 	public String getContrasena();
 	public String getNombre();
-
+    
+	/*
+	 * METODOS LOGIN 
+	 * */
+    void iniciarSesion(String nombreUsuario, String contraseña);
+    void cerrarSesion();
+    boolean estaSesionIniciada();
 
 }
